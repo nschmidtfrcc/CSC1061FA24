bd81d490a527e563acd1d9f87989bc5a2734eb3c
Emilio <emiliopinales@icloud.com>
2024-10-29 09:38:28 -0600

    add display info function and gross sales

---
+++ displayinfo.cpp
@@ -0,0 +1,27 @@
+#include <iostream> 
+
+using namespace std; 
+
+ int main () { 
+
+    }//end main EP
+
+   void car::displayAvailbleCarInfo() { 
+      cout << "availble car will be displayed in the following order: " << endl;
+      cout << "Price, year, make, model, and vin. " << endl; 
+      cout << Price << Year << Make << Model << Vin; 
+      } 
+   void car::displayUnavailbleCarInfo() { 
+      cout << "unavailble car will be displayed in the following order: " << endl;
+      cout << "Price, year, make, model, and vin. " << endl; 
+            cout << Price << Year << Make << Model << Vin; 
+      } 
+    double grossSales () { 
+    double totalSales = 0.0; 
+    for () { 
+    if (car.isSold) { 
+     totalSales += car.price;
+     }//
+     }//
+     return totalSales; 
+     }//end double grossSales 
