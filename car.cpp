aac551982ebc7f1f8685b88d553ac482d862e317
ibseyer <83263711+ibseyer@users.noreply.github.com>
2024-10-31 09:21:33 -0600

    Update car.cpp
    added code from CAR.cpp to car.cpp

--- car.cpp
+++ car.cpp
@@ -1,1 +1,24 @@
+#include "car.h"
 
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+Car::Car(double price, int year, string make, string model, string available, int vin) {
+   carPrice = price;
+   carYear = year;
+   carMake = make;
+   carModel = model;
+   carAvailable = available;
+   carVin = vin;
+}
+
+void Car::displayInfo() {
+   cout << "Car Price: $" << carPrice;
+   cout << "\nCar Year: " << carYear;
+   cout << "\nCar Make and Model: " << carMake << " " << carModel;
+   cout << "\nIs Car Available? " << carAvailable;
+   cout << "\nCar VIN: " << carVin;
+}
