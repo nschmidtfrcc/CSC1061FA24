2dc9d8e14a55dd4c03142c46b6c29e9d1fc6ac89
Isaac Seyer <iseyer@student.cccs.edu>
2024-10-31 08:34:47 -0600

    Added the IPO comments for the main function

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 
-/*
+/* displayMenu
 Name: Isaac Seyer
 
 Input: The user will input an integer between 1 and 6 (inclusive) to choose an option
@@ -35,6 +35,25 @@ int displayMenu() {
    return choice;
 
 }// end menuDisplay IS
+
+/*     CarDealershipMain
+Input: The program will expect numbers as input to traverse the menus and reach the desired function. 
+   Then the user will be prompted for specific input within each function. Input required in the search functionality 
+   is the make, model or year being searched for. Input required in the sell car functionality is the VIN number.
+   Input of a 6 is also required to exit the program
+   
+Process: This program will populate two arrays with the information of the unsold and sold cars from external files
+   Then the program will expect numbers as input to traverse the menus and reach the desired function. 
+   Then the user will be prompted for specific input within each function. This programs functions
+   include: A function that displays the information of the sold and unsold cars. Searching the lists of sold 
+   and unsold cars to find cars based on make, model, and yea. Another function to sell a car moving it
+   from the unsold list to the sold car list. Then the function to display the Gross Sales for all of the 
+   cars sold during this instance of the program. Then whenever the program is exitted, the inventory of the 
+   sold and unsold cars is exportted to external files.
+
+Output:
+
+*/
 
 int main(int argc, char* argv[]) {
    //Declarations IF
