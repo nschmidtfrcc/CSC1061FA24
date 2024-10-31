0d2ba4708816d3872488926dd89d350b67bc360d
CharC <catspam3r@gmail.com>
2024-10-31 09:42:50 -0600

    updated the class CPP file

--- car.h
+++ car.h
@@ -7,12 +7,15 @@ using namespace std;
 
 class Car {
    public:
+      Car();
       Car(int price, int year, string make, string model, bool sold, int vin);
       void carDetails();
       int getPrice();
       int getYear();
       string getMake();
       string getModel();
+      void setSold(bool sold);
+      bool getSold();
       int getVin();
    private:
       int carPrice;
