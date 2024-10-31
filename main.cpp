958ea7807889468199ade1e1323557e38bed9e8d
ibseyer <83263711+ibseyer@users.noreply.github.com>
2024-10-31 08:17:21 -0600

    merged Main.cpp and main.cpp into main.cpp

--- main.cpp
+++ main.cpp
@@ -36,6 +36,40 @@ int menuDisplay() {
 
 }// end menuDisplay IS
 
-int main(int argc, char *argv[]) {
-   return 0;
-}// end main IS
+int main(int argc, char* argv[]) {
+   //Declarations IF
+   int userChoice;
+   bool usingProgram = true;
+   
+   //Load in car inventory information
+   
+   //Display menu
+   while(usingProgram) {
+      switch (displayMenu()) {
+         case 1:
+            break;
+         case 2:
+            break;
+         case 3:
+            break;
+         case 4:
+            break;
+         case 5:
+            break;
+         case 6:
+            usingProgram = false;
+            break;
+         default:
+            cout << "displayMenu returned an integer that was not a selection" << endl;
+            usingProgram = false;
+            break;
+      }//end switch
+   }//end while
+   
+
+   
+   //repeat
+   
+   //export car inventory information
+
+}//end main
